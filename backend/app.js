b590aa7510636396e3ea8a7d6bad80739eb24c0f
Leslie <flandre0815@gmail.com>
2026-02-08 14:33:25 +0800

    test2

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,6 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/healthcheck", (req, res) => {
-  res.status(200);
   res.status(200).json({
     status: "success",
     message: "OK",
